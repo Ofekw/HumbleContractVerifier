2af670d12d65576a49a06143adf8745e997f1ff7
fancycat <lukedude@gmail.com>
2021-06-21 14:59:29 -0700

    Move ISwapBase

---
+++ Library/Interfaces/ISwapBase.cs
@@ -0,0 +1,14 @@
+﻿namespace HumbleVerifierLibrary
+{
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    public interface ISwapBase
+    {
+        BigInteger amountIn { get; set; }
+        BigInteger amountOutMin { get; set; }
+        IList<string> path { get; set; }
+        string to { get; set; }
+        BigInteger deadline { get; set; }
+    }
+}
